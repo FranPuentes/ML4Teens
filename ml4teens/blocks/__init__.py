6ee41ad10a39481ac4408c3c13ad12870e77dc76
FranPuentes <fran@puentescalvo.com>
2024-01-03 18:21:20 +0100

    Nuevos bloques para SAM y estimación de la profundidad

--- ml4teens/blocks/__init__.py
+++ ml4teens/blocks/__init__.py
@@ -6,9 +6,12 @@ from .convertImage      import ConvertImage
 from .cropImage         import CropImage
 from .resizeImage       import ResizeImage
 from .imageSegmentation import ImageSegmentation
+from .imageSegmentAll   import ImageSegmentAll
 from .imageOp           import ImageOp
 from .display           import Display
 from .terminal          import Terminal
 from .videoSource       import VideoSource
 from .imageSource       import ImageSource
 from .objectID          import ObjectID
+from .imageToText       import ImageToText
+from .depthEstimation   import DepthEstimation
