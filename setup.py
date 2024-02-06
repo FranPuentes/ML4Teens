71a35f94356100f9db1e8e381f13d21257c2920f
FranPuentes <fran@puentescalvo.com>
2024-02-07 00:23:59 +0100

    Versión 1.29

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ml4teens',
-    version='0.1.28',
+    version='0.1.29',
     packages=find_packages(),
     install_requires=[ "ipython", 
                        "opencv-python>=4.9", 
@@ -14,7 +14,7 @@ setup(
                        "ipyevents",
                        "ipywidgets",
                        "pandas>=1.3.4",
-                       "transformers>=4.38.0.dev0",
+                       "transformers>=4.38",
                        "torch>=2.2.0",
                        "mediapipe>=0.10.9",
                        "jupyter_ui_poll",
