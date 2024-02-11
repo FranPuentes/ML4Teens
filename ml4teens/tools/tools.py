c4619b0bbac7c6b3de0877698f32a09367e046b5
FranPuentes <fran@puentescalvo.com>
2024-02-11 19:49:40 +0100

    ...

--- ml4teens/tools/tools.py
+++ ml4teens/tools/tools.py
@@ -2,10 +2,14 @@ from PIL import Image;
 import requests;
 from io import BytesIO;
 
-# TODO parámetros opciones, modo y tamaño
-def image_from_url(url:str, mode=None, width=None, height=None):
+#===============================================================================
+def image_from_url(url:str, mode:str=None, width:int=None, height:int=None):
     """
     Esta función recibe como parámetro una url:str y si es posible devuelve una imagen:Image.
+    
+    Si se indica el mode:str, se convertirá la imagen a dicho formato.
+    
+    Si se indica width:int o height:int, se redimensionará la imagen.
     
     Si no puede devolver la imagen dispara una excepción.
     """
@@ -15,7 +19,31 @@ def image_from_url(url:str, mode=None, width=None, height=None):
       response = requests.get(url, headers=headers);      
       if response.status_code == 200:
          img = Image.open(BytesIO(response.content));
-         print(f"Formato: {img.format}, Tamaño: {img.size}, Modo: {img.mode}");
+         
+         img.load();
+         
+         if width is not None or height is not None:
+         
+            original_width, original_height = img.size
+         
+            if width is not None and height is None:
+               ratio = width / original_width;
+               new_height = int(original_height * ratio);
+               new_dimensions = (width, new_height);
+           
+            elif height is not None and width is None:
+               ratio = height / original_height
+               new_width = int(original_width * ratio)
+               new_dimensions = (new_width, height)
+           
+            else:
+               new_dimensions = (original_width, original_height)
+               
+            img = img.resize(new_dimensions, Image.ANTIALIAS);
+         
+         if mode is not None: return img.convert(mode);
+         else:                return img;
+         
       else:
          raise RuntimeError(f"Error al descargar la imagen: Estado HTTP {response.status_code}");
         
