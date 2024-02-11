fa54a612c279715d0e54f4a9bc47b75e5da2e8f8
FranPuentes <fran@puentescalvo.com>
2024-02-11 19:35:06 +0100

    ...

--- ml4teens/tools/tools.py
+++ ml4teens/tools/tools.py
@@ -2,16 +2,20 @@ from PIL import Image;
 import requests;
 from io import BytesIO;
 
-def image_from_url(url:str):
+# TODO parámetros opciones, modo y tamaño
+def image_from_url(url:str, mode=None, width=None, height=None):
     """
     Esta función recibe como parámetro una url:str y si es posible devuelve una imagen:Image.
     
     Si no puede devolver la imagen dispara una excepción.
     """
     try:
-        response = requests.get(url);
-        img = Image.open(BytesIO(response.content))
-        return img;
+      response = requests.get(url);
+      if response.status_code == 200:
+         img = Image.open(BytesIO(response.content));
+         print(f"Formato: {img.format}, Tamaño: {img.size}, Modo: {img.mode}");
+      else:
+         raise RuntimeError(f"Error al descargar la imagen: Estado HTTP {response.status_code}");
         
     except Exception as e:
         raise RuntimeError(f"No he podido cargar la siguiente imagen: {url}");
