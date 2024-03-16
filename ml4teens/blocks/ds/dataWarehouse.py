81a66c1a6a12beb8996f9b9e99cf24ea6a8432dc
FranPuentes <fran@puentescalvo.com>
2024-03-16 17:58:48 +0100

    blocks.ds.DataWarehouse

--- ml4teens/blocks/ds/dataWarehouse.py
+++ ml4teens/blocks/ds/dataWarehouse.py
@@ -1,10 +1,83 @@
 
+import copy;
+import pandas as pd;
+
 from ...core import Block;
 
 #===============================================================================
 class DataWarehouse(Block):
-
+      """
+      Almacén de datos estructurados.
+      Básicament un wrap alrededor de un DataFrame de Pandas.
+      Permite:
+      * leer y escribir en/a un fichero.
+      * recibir 'features' en formato "lista de diccionarios con claves 'name' y 'score'", cada lista es una instancia.
+      * recibir una etiqueta, que será la etiqueta de los features recibidos mientras no reciba otra.
+      
+      'name' será de tipo string.
+      'score' será numérico. Si es de tipo string se intentará convertir a número, si no es posible se almacena.
+      
+      Parámetros:
+      * self.params.filename: nombre del fichero por defecto.
+      * self.params.label_name: nombre de la etiqueta por defecto.
+      * self.params.label: valor de la etiqueta por defecto.
+      
+      TODO:
+      * recibir features en otros formatos
+      """
+      
       #-------------------------------------------------------------------------
       def __init__(self, **kwargs):
           super().__init__(**kwargs);
+          
+          self._df=pd.DataFrame();
+          self._label=None;
+          
+      #-------------------------------------------------------------------------
+      @property
+      def dataframe(self):
+          return self._df;
 
+      #-------------------------------------------------------------------------
+      def load(self, filename=None):
+          self._df=pd.read_csv(filename if bool(filename) else self.params.filename);
+      
+      #-------------------------------------------------------------------------
+      def save(self, filename=None):
+          self._df.to_csv(filename if bool(filename) else self.params.filename, index=False);
+      
+      #-------------------------------------------------------------------------
+      @Block.slot("label", {int, float, str})
+      def slot_label(self, slot, data):
+
+          self._label=data;
+
+          if self._label is not None and self.params.labelName is None:
+             self.params.labelName="__TARGET__";             
+          
+      #-------------------------------------------------------------------------
+      @Block.slot("instance", {list})
+      def slot_features(self, slot, data):
+      
+          if isinstance(data,list) and all(isinstance(d,dict) and all(key in d for key in ["name","score"]) for d in data):
+             # lista de diccionarios con claves 'name' y 'score'.
+             inst={};
+             for item in data:
+                 inst[data["name"]]=data["score"];
+             if self.params.labelName:
+                inst[self.params.labelName]=self._label if self._label is not None else self.params.label;
+             self._df = pd.concat( [ self._df, pd.DataFrame([inst]) ], ignore_index=True);
+             
+          if isinstance(data,dict):
+             # diccionario, las claves son las columnas.
+             inst=copy.copy(data);
+             if self.params.labelName:
+                inst[self.params.labelName]=self._label if self._label is not None else self.params.label;
+             self._df = pd.concat( [ self._df, pd.DataFrame([inst]) ], ignore_index=True);
+             
+          self.signal_dataframe(self._df);
+
+      #-------------------------------------------------------------------------
+      @Block.signal("dataframe", {pd.DataFrame})
+      def signal_dataframe(self, data):
+          return data;
