7940f807b4372abdba8db8ad5a0b763c7086f85e
FranPuentes <fran@puentescalvo.com>
2024-03-16 19:43:37 +0100

    DataLabelizer, funciona!

--- ml4teens/blocks/ds/dataLabelizer.py
+++ ml4teens/blocks/ds/dataLabelizer.py
@@ -1,6 +1,8 @@
 
 import ipywidgets as widgets;
+
 from IPython.display import display;
+from ipywidgets      import Layout, Button, HBox;
 
 from ...core import Block;
 
@@ -14,17 +16,25 @@ class DataLabelizer(Block):
       def __init__(self, **kwargs):
           super().__init__(**kwargs);
           
-          self._labels=None;
-          self._data  =None;          
+          self._labels=self.params.labels if self.params.labels is not None else ["Sí","No"];
+          self._data  =None;
           
-          button = widgets.Button(description="Hola!");
-          button.style.button_color = 'lightgreen';  # Cambiar el color de fondo del botón
-          button.style.font_weight = 'bold';         # Cambiar el peso de la fuente a negrita
-          button.layout.width = '200px';             # Cambiar el ancho del botón
-          button.layout.height = '40px';             # Cambiar la altura del botón
-          button.layout.border = '2px solid black';  # Agregar un borde al botón
-          button.on_click(lambda _: None);
-          display(button);
+          buttons=[];
+          for n,label in enumerate(self._labels):
+              button = Button(description=str(label), layout=Layout(width='auto') );
+              button.style.button_color = 'Orange';      # Cambiar el color de fondo del botón
+              button.layout.width = '200px';             # Cambiar el ancho del botón
+              button.layout.height = '30px';             # Cambiar la altura del botón
+              button.on_click(lambda _, label=label: self.click_label(label));
+              buttons.append(button);
+          
+          hbox=HBox(buttons, layout=Layout(border='solid'));
+          display(hbox);
+          
+      #-------------------------------------------------------------------------
+      def click_label(self, label):
+          self.signal_label(str(label));
+          self.signal_data(self._data);
           
       #-------------------------------------------------------------------------
       @Block.slot("data", {object})
