60d78b29f95d7504991fd2bf100394eef4874d63
FranPuentes <fran@puentescalvo.com>
2024-03-17 19:40:33 +0100

    Nuevo bloque: Values

---
+++ ml4teens/blocks/values.py
@@ -0,0 +1,20 @@
+from ..core import Block;
+
+class Values(Block):
+
+      def __init__(self, **kwargs):
+          super().__init__(**kwargs);
+          self._data=[];
+
+      @Block.slot("data", {object})
+      def slot_data(self, slot, data):
+          self._data.append(data if data is not None else self.params.default);
+          self.signal_data(data);
+
+      @Block.signal("data", object)
+      def signal_data(self, data):
+          return data;
+
+      @property
+      def data(self):
+          return self._data;
