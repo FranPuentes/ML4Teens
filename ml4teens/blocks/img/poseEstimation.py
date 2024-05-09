c2ec19394913d4561686567e7615fb7809efd715
FranPuentes <fran@puentescalvo.com>
2024-05-09 10:53:24 +0200

    Detección de poses cuando no las hay.

--- ml4teens/blocks/img/poseEstimation.py
+++ ml4teens/blocks/img/poseEstimation.py
@@ -42,49 +42,53 @@ class PoseEstimation(Block):
       #-------------------------------------------------------------------------
       @Block.slot("image", {Image})
       def slot_image(self, slot, data):
-          if data is not None:
-             results = self._model.predict(data, verbose=False, **self._rest);
-             for result in results:
-                 if self.signal_boxes():
-                                  
-                    _boxes     = result.boxes;
-                    _keypoints = result.keypoints;
-                    
-                    boxes=[{
-                            "kind":(int(r),self._model.names[int(r)]),
-                            "trust":float(c),
-                            "xyxy":[float(_) for _ in k],
-                            "kp":[[float(_) for _ in l] for l in p],
-                           }
-                           for r,c,k,p in zip(_boxes.cls,_boxes.conf,_boxes.xyxyn,_keypoints.xyn)];
 
-                    self.signal_boxes(boxes);
-                    
-                 if self.signal_features():
-                    
-                    for conf,xyn in zip(result.keypoints.conf.cpu(), result.keypoints.xyn.cpu()):
-                        cv=[];
-                        mc = torch.mean(xyn, dim=0);
-                        for i, point in enumerate(xyn):
-                            if conf[i] >= (self.params.verge or 0.5):
-                               d=math.sqrt((point[0]-mc[0])**2 + (point[1]-mc[1])**2);
-                               cv.append(d);
-                            else:
-                               cv.append(None);                               
-                        
-                        cv = np.array(cv, dtype=float);
-                        mean_value = np.nanmean(cv);
-                        mask=np.isnan(cv);
-                        cv[mask] = mean_value;
-                        cv = cv /  np.linalg.norm(cv);
-                        cv[mask] = np.nan;
-                        
-                        self.signal_features(cv); 
-                    
-                 if self.signal_image():
-                    image = result.plot(conf=False, labels=False, boxes=False, masks=False, probs=False);
-                    image = PIL.Image.fromarray(image[..., ::-1]);
-                    self.signal_image(image);
+          if data is not None:
+
+             results = self._model.predict(data, verbose=False, **self._rest);
+
+             for result in results:
+
+                 if result.boxes is not None and result.keypoints.conf is not None and result.keypoints.xyn is not None:
+
+                    if self.signal_boxes():                                    
+                      _boxes     = result.boxes;
+                      _keypoints = result.keypoints;
+                      
+                      boxes=[{
+                              "kind":(int(r),self._model.names[int(r)]),
+                              "trust":float(c),
+                              "xyxy":[float(_) for _ in k],
+                              "kp":[[float(_) for _ in l] for l in p],
+                             }
+                             for r,c,k,p in zip(_boxes.cls,_boxes.conf,_boxes.xyxyn,_keypoints.xyn)];
+
+                      self.signal_boxes(boxes);
+                      
+                    if self.signal_features():                      
+                       for conf,xyn in zip(result.keypoints.conf.cpu(), result.keypoints.xyn.cpu()):
+                           cv=[];
+                           mc = torch.mean(xyn, dim=0);
+                           for i, point in enumerate(xyn):
+                               if conf[i] >= (self.params.verge or 0.5):
+                                  d=math.sqrt((point[0]-mc[0])**2 + (point[1]-mc[1])**2);
+                                  cv.append(d);
+                               else:
+                                  cv.append(None);                               
+                          
+                           cv = np.array(cv, dtype=float);
+                           mean_value = np.nanmean(cv);
+                           mask=np.isnan(cv);
+                           cv[mask] = mean_value;
+                           cv = cv /  np.linalg.norm(cv);
+                           cv[mask] = np.nan;
+                          
+                           self.signal_features(cv); 
+                       
+                    if self.signal_image():
+                       image = result.plot(conf=False, labels=False, boxes=False, masks=False, probs=False);
+                       image = PIL.Image.fromarray(image[..., ::-1]);
+                       self.signal_image(image);
 
       #-------------------------------------------------------------------------
       @Block.signal("image", Image)
